309c3e8b24a09ddce751ad06b0093523c3f85c97
yuqiaodan <yuqiaodan@testbird.com>
2023-11-17 13:41:47 +0800

    自定义view 1

---
+++ app/src/main/java/com/tomato/amelia/customview/MyCircleView.kt
@@ -0,0 +1,24 @@
+package com.tomato.amelia.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+
+/**
+ * author: created by yuqiaodan on 2023/11/17 10:40
+ * description:
+ * 自定义View步骤：
+ * 1.
+ */
+class MyCircleView:View{
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+
+
+
+
+}
+
